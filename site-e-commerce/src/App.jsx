9acef9ed44f3839738cdc69e30b241088c231b83
Saif Ali <kattan.saifali@gmail.com>
2024-03-14 14:57:41 +0100

    add:ajouter route produits

--- site-e-commerce/src/App.jsx
+++ site-e-commerce/src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homepage from './Views/Homepage';
-import Panier from './Views/Panier';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Views/Homepage";
+import Panier from "./Views/Panier";
+import Produits from "./Views/Produits";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-      <Route element={<Homepage />} path="/" />
-      <Route element={<Panier />} path="/panier" />
-      </Routes>
+        <Routes>
+          <Route element={<Homepage />} path="/" />
+          <Route element={<Panier />} path="/panier" />
+          <Route element={<Produits />} path="/Produits"></Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
