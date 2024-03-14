0ffbd5ce03939968a0ee0bf7cf158c98ba3b91b2
Saif Ali <kattan.saifali@gmail.com>
2024-03-14 14:59:05 +0100

    add:ajouter les produits et aussi les images

--- site-e-commerce/src/Components/CardProduit.jsx
+++ site-e-commerce/src/Components/CardProduit.jsx
@@ -1,41 +1,95 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import image1 from "../assets/image1.png";
+import image2 from "../assets/image2.png";
+import image3 from "../assets/image3.png";
+import image4 from "../assets/image4.png";
 
 const CardProduit = () => {
-    return (
-        <>
-            <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body style={styles.cardText}>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="success">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-        </>
-    );
+  return (
+    <div style={styles.container}>
+      <Card style={styles.card}>
+        <Image src={image1} fluid />
+        {/* <Card.Img variant="top" src="../../public/image1.png" /> */}
+        <Card.Body style={styles.cardText}>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button style={styles.button}>Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+      <Card style={styles.card}>
+        <Image src={image2} fluid />
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Card.Body style={styles.cardText}>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button style={styles.button}>Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <Card style={styles.card}>
+        <Image src={image3} fluid />
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Card.Body style={styles.cardText}>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button style={styles.button}>Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <Card style={styles.card}>
+        <Image src={image4} fluid />
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Card.Body style={styles.cardText}>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button style={styles.button}>Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 const styles = {
-    card: {
-        width: '18rem',
-        margin: '20px', // Exemple de style supplémentaire
-        boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)', // Exemple de style supplémentaire
-    },
-    cardText: {
-        fontSize: '16px', // Exemple de style pour le texte dans la card
-        color: '#333', // Exemple de couleur du texte
-    },
-    button: {
-        backgroundColor: '#007bff', // Exemple de couleur d'arrière-plan du bouton
-        borderColor: '#007bff', // Exemple de couleur de bordure du bouton
-        borderRadius: '5px', // Exemple de style pour arrondir les coins du bouton
-        marginTop: '10px', // Exemple de marge supérieure pour le bouton
-    },
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    margin: "0 50px",
+    gap: "20px",
+    paddingTop: "50px",
+  },
+
+  card: {
+    width: "18rem",
+    margin: "20px", // Exemple de style supplémentaire
+    boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", // Exemple de style supplémentaire
+  },
+  cardText: {
+    fontSize: "16px", // Exemple de style pour le texte dans la card
+    color: "#333", // Exemple de couleur du texte
+  },
+  button: {
+    backgroundColor: "#007bff", // Exemple de couleur d'arrière-plan du bouton
+    borderColor: "#007bff", // Exemple de couleur de bordure du bouton
+    borderRadius: "5px", // Exemple de style pour arrondir les coins du bouton
+    marginTop: "30px", // Exemple de marge supérieure pour le bouton
+    marginBottom: "20px",
+    flex: "1",
+  },
 };
 
 export default CardProduit;
--- site-e-commerce/src/Components/NavigationBar.jsx
+++ site-e-commerce/src/Components/NavigationBar.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { FaBasketShopping } from "react-icons/fa6";
 
 const Navigationbar = () => {
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">Idée de nom ?</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Accueil</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#panier">
-                          <FaBasketShopping  size={30} />
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Idée de nom ?</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Accueil</Nav.Link>
+          </Nav>
+          <Nav className="me-auto">
+            <Nav.Link href="/Produits">Produits</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#panier">
+              <FaBasketShopping size={30} />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Navigationbar;
