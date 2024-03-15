e44578534d03393d657337d704e79dab76c1efe1
Saif Ali <kattan.saifali@gmail.com>
2024-03-15 15:40:04 +0100

    add:ajouter nouveau modal

--- site-e-commerce/src/Components/AjouterModal.jsx
+++ site-e-commerce/src/Components/AjouterModal.jsx
@@ -1,19 +1,46 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
-const AjouterModal = ({show, handleClose}) => {
-
-    return (
-        <div
+const AjouterModal = ({ show, handleClose }) => {
+  return (
+    <div
       className="modal show"
-      style={{ display: 'block', position: 'initial' }}
+      style={{ display: "block", position: "initial" }}
     >
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Ajouter un Modele</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          {" "}
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            {/* <Form.Label>Password</Form.Label> */}
+            <Form.Control type="text" placeholder="Title" />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Body>
+          {" "}
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            {/* <Form.Label>Password</Form.Label> */}
+            <Form.Control type="text" placeholder="Image" />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Body>
+          {" "}
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            {/* <Form.Label>Password</Form.Label> */}
+            <Form.Control type="text" placeholder="Prix" />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Body>
+          {" "}
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            {/* <Form.Label>Password</Form.Label> */}
+            <Form.Control as="textarea" rows={5} placeholder="Description" />
+          </Form.Group>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
@@ -24,7 +51,7 @@ const AjouterModal = ({show, handleClose}) => {
         </Modal.Footer>
       </Modal>
     </div>
-    );
+  );
 };
 
 export default AjouterModal;
